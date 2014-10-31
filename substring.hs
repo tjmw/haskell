e53fcfe77b19013ac1f617d2fec539f8496cbaf0
Tom Wey <tom@new-bamboo.co.uk>
2014-10-31 14:21:06 +0000

    Better naming

--- substring.hs
+++ substring.hs
@@ -1,12 +1,12 @@
 substring :: [Char] -> [Char] -> Bool
 substring [] _ = False
 substring _ [] = False
-substring xs (y:ys) = if length xs <= length ([y] ++ ys) && eql_pairs (zip xs ([y] ++ ys))
+substring s (h:t) = if length s <= length ([h] ++ t) && eql_pairs (zip s ([h] ++ t))
                       then True
-                      else substring xs ys
+                      else substring s t
 
 eql_pairs :: [(Char,Char)] -> Bool
-eql_pairs xs = length [x | x <- xs, eql_pair x] == length xs
+eql_pairs ps = length [p | p <- ps, eql_pair p] == length ps
 
 eql_pair :: (Char,Char) -> Bool
 eql_pair (a,b) = a == b
